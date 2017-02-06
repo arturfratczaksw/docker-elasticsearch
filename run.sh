7c706ce96f6da693182baea0ca00fc2e3329749c
AF <artur.fratczak@smoothwall.net>
2017-02-06 11:23:57 +0000

    Added a line to alter the max_map_count limit.
    Elasticsearch requires vm.max_map_count set to min of 262144, otherwise it
    fails to start in kubernetes cluster. I've tried to preset the value with
    init-containers which do not seem to work for replication controllers (they're
    still in beta accessible only through annotations).
    
    Change here just sets these values to the min. requires rebuilding both
    docker-elasticsearch and docker-elasticsearch-kubernetes which uses this
    image as a base.

--- run.sh
+++ run.sh
@@ -8,7 +8,10 @@ chown -R elasticsearch /elasticsearch /data
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
 # allow for memlock
+echo "Resetting Limits: "
 ulimit -l unlimited
+sudo sysctl -w vm.max_map_count=262144
+echo "Done"
 
 # run
 sudo -E -u elasticsearch /elasticsearch/bin/elasticsearch
